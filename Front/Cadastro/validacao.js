5d244290635a7859c766fdd351e6ab1aaacf7e65
Andre Luiz J Poles <andre.poles@fatec.sp.gov.br>
2024-04-02 20:32:48 -0300

    Remove campo CPF

--- Front/Cadastro/validacao.js
+++ Front/Cadastro/validacao.js
@@ -1,6 +1,5 @@
 const email = document.getElementById("email");
 const nome = document.getElementById("nome");
-const cpf = document.getElementById("cpf");
 const dataNasc = document.getElementById("data-nasc");
 const senha = document.getElementById("senha");
 const confSenha = document.getElementById("confirmar-senha");
@@ -35,7 +34,7 @@ function validar() {
 }
 
 function verificaVazio() {
-    return email.value == '' || nome.value == '' || cpf.value == '' || dataNasc.value == '' || senha.value == '' || confSenha.value == ''
+    return email.value == '' || nome.value == '' || dataNasc.value == '' || senha.value == '' || confSenha.value == ''
 }
 
 function validaEmail(){
@@ -43,37 +42,6 @@ function validaEmail(){
         return false;
     }
     return true;
-}
-
-function validaCpf() {
-    strCPF = cpf.value;
-    strCPF = strCPF.replace(/[^\d]+/g, '');
-    var soma = 0, resto = 0;
-    if (strCPF == "00000000000" ||
-        strCPF == "11111111111" ||
-        strCPF == "22222222222" ||
-        strCPF == "33333333333" ||
-        strCPF == "44444444444" ||
-        strCPF == "55555555555" ||
-        strCPF == "66666666666" ||
-        strCPF == "77777777777" ||
-        strCPF == "88888888888" ||
-        strCPF == "99999999999") return true;
-
-    for (i = 1; i <= 9; i++) soma = soma + parseInt(strCPF.substring(i - 1, i)) * (11 - i);
-    resto = (soma * 10) % 11;
-
-    if ((resto == 10) || (resto == 11)) resto = 0;
-    if (resto != parseInt(strCPF.substring(9, 10))) return true;
-
-    soma = 0;
-    for (i = 1; i <= 10; i++) soma = soma + parseInt(strCPF.substring(i - 1, i)) * (12 - i);
-    resto = (soma * 10) % 11;
-
-    if ((resto == 10) || (resto == 11)) resto = 0;
-    if (resto != parseInt(strCPF.substring(10, 11)))return true;
-    
-    return false && strCPF.value.length< 11;
 }
 
 function comparaSenha(){
