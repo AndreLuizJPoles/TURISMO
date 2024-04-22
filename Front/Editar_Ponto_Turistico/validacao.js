812918c3c61cb8d94afb97310bb29fec9fa26917
André Luiz J Poles <110946640+AndreLuizJPoles@users.noreply.github.com>
2024-04-22 10:33:19 -0300

    Editar ponto evento (#13)
    * Tela Editar Ponto Turístico
    
    * tela de editar_evento

---
+++ Front/Editar_Ponto_Turistico/validacao.js
@@ -0,0 +1,45 @@
+const nomeEstab = document.getElementById("nome");
+const horaAberto = document.getElementById("hora-abertura");
+const horaFecha = document.getElementById("hora-encerramento");
+const segunda = document.getElementById("segunda");
+const terca = document.getElementById("terca");
+const quarta = document.getElementById("quarta");
+const quinta = document.getElementById("quinta");
+const sexta = document.getElementById("sexta");
+const sabado = document.getElementById("sabado");
+const domingo = document.getElementById("domingo");
+const cep = document.getElementById("cep");
+const uf = document.getElementById("uf");
+const cidade = document.getElementById("cidade");
+const bairro = document.getElementById("bairro");
+const rua = document.getElementById("rua");
+const numero = document.getElementById("numero");
+const complemento = document.getElementById("complemento");
+
+
+function salvar() {
+    if (validar()) {
+        //envia(); VER COM O ESDRAS
+        alert("FOI!")
+    }
+}
+
+function validar() {
+    if (verificaVazio()) {
+        alert("Preencha todos os campos obrigatórios!");
+        return false;
+    }
+    if(validaHora()){
+        alert("O horário de abertura deve ser menor que o horário de fechamento!")
+        return false;
+    }
+    return true;
+}
+
+function verificaVazio() {
+    return nomeEstab.value == '' || horaAberto.value == '' || horaFecha.value == '' || cep.value == '' || cidade.value == '' || bairro.value == '' || rua.value == '' || uf.value =='';
+}
+
+function validaHora(){
+    return horaFecha.value <= horaAberto.value;
+}
