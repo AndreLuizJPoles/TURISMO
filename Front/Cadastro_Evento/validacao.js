0458ef6db31cd9a5e1c968f926979aafc79afacf
André Luiz J Poles <110946640+AndreLuizJPoles@users.noreply.github.com>
2024-04-22 10:19:32 -0300

    Cad evento (#12)
    * Add botao de Meus Eventos no Perfil do Usuario
    
    * Add e arruma Informacoes de Funcionamento
    
    * Add todos os campos necessarios
    
    * Add validacao

---
+++ Front/Cadastro_Evento/validacao.js
@@ -0,0 +1,44 @@
+const nome = document.getElementById("nome");
+const estabelecimento = document.getElementById("estabelecimento");
+const descricao = document.getElementById("descricao");
+const dataInicio = document.getElementById("data-inicio");
+const dataFim = document.getElementById("data-fim");
+const horaAberta = document.getElementById("hora-abertura");
+const horaEncer = document.getElementById("hora-encerramento");
+
+function cadastrar() {
+    if (validar()) {
+        alert('Foi!');
+        //envia();
+    }
+}
+
+function validar(){
+    if (verificaVazio()) {
+        alert("Preencha todos os campos obrigatórios!");
+        return false;
+    }
+    if(validaHora()){
+        alert("O horário de abertura deve ser menor que o horário de fechamento!")
+        return false;
+    }
+
+    if(validaData()){
+        alert("A data de início deve ser menor que a data de encerramento!")
+        return false;
+    }
+
+    return true;
+}
+
+function verificaVazio() {
+    return nome.value == '' || horaAberta.value == '' || horaEncer.value == '' || dataInicio.value == '' || dataFim.value == '' || estabelecimento.value == '';
+}
+
+function validaHora(){
+    return horaEncer.value <= horaAberta.value;
+}
+
+function validaData(){
+    return dataFim.value <= dataInicio.value;
+}
