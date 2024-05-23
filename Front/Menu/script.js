7c6fb52b9cd9c8decea6fe7595eb933f1e2202fa
Andre Luiz J Poles <andre.poles@fatec.sp.gov.br>
2024-05-23 14:53:20 -0300

    Add integracao nos menus laterais

--- Front/Menu/script.js
+++ Front/Menu/script.js
@@ -1,5 +1,33 @@
+const nome_user = document.getElementById('nome-usuario');
+const endereco = document.getElementById('endereco');
+
 window.onload = async function () {
+
     const ID = await pegaID();
+    const LOCAL_API_URL_USER = `http://localhost:3000/api/users/${ID}`;
+
+    try {
+
+        console.log(LOCAL_API_URL_USER);
+
+        const response = await axios.get(
+            LOCAL_API_URL_USER,
+            {
+                headers: {
+                    authorization: `Bearer ${localStorage.getItem("token")}`,
+                },
+            }
+        );
+
+        console.log(response);
+
+        nome_user.innerHTML = response.data.data.name;
+        endereco.innerHTML = `<img src="../Perfil_Usuario/imgs/pin.png" id="icon-endereco" class="icon"> <p id="end-texto">${response.data.data.address}`;
+
+    } catch (error) {
+        console.log(error);
+    }
+
     const LOCAL_API_URL = `http://localhost:3000/api/establishments`;
 
     try {
@@ -64,11 +92,11 @@ window.onload = async function () {
 }
 
 async function pegaID() {
-    const LOCAL_API_URL = 'http://localhost:3000/api/users/loggedUser';
+    const LOCAL_API_URL_ID = 'http://localhost:3000/api/users/loggedUser';
     try {
 
         const response = await axios.get(
-            LOCAL_API_URL,
+            LOCAL_API_URL_ID,
             {
                 headers: {
                     authorization: `Bearer ${localStorage.getItem("token")}`,
