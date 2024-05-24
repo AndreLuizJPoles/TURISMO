cbefe98724f5005220676cb225a40abdb2bc0326
Andre Luiz J Poles <andre.poles@fatec.sp.gov.br>
2024-05-24 13:38:42 -0300

    integra grande parte do perfil de estabelecimento

--- Front/Perfil_Estabelecimento/script.js
+++ Front/Perfil_Estabelecimento/script.js
@@ -1,5 +1,15 @@
 const nome_user = document.getElementById('nome-usuario');
 const endereco = document.getElementById('endereco');
+const nome = document.getElementById('nome');
+const enderecoEstab = document.getElementById('enderecoText');
+const descricao = document.getElementById('desc');
+const horario = document.getElementById('horario');
+const novaPost = document.getElementById('nova-postagem');
+const titulo = document.getElementById('titulo-pagina');
+const perfilFoto = document.getElementById('perfil-foto');
+const planoFundo = document.getElementById('plano-fundo');
+const editarEst = document.getElementById('editar-est');
+const iconeEditar = document.getElementsByClassName('icone-editar');
 
 window.onload = async function () {
 
@@ -7,9 +17,6 @@ window.onload = async function () {
     const LOCAL_API_URL_USER = `http://localhost:3000/api/users/${ID}`;
 
     try {
-
-        console.log(LOCAL_API_URL_USER);
-
         const response = await axios.get(
             LOCAL_API_URL_USER,
             {
@@ -23,6 +30,41 @@ window.onload = async function () {
 
         nome_user.innerHTML = response.data.data.name;
         endereco.innerHTML = `<img src="../Perfil_Usuario/imgs/pin.png" id="icon-endereco" class="icon"> <p id="end-texto">${response.data.data.address}`;
+
+    } catch (error) {
+        console.log(error);
+    }
+
+    const idEstab = localStorage.getItem("idEstab");
+    const LOCAL_API_URL = `http://localhost:3000/api/establishments/${idEstab}`;
+
+    try {
+        const response = await axios.get(
+            LOCAL_API_URL,
+            {
+                headers: {
+                    authorization: `Bearer ${localStorage.getItem("token")}`,
+                },
+            }
+        );
+
+        console.log(response);
+
+        nome.innerHTML = response.data.data.name;
+        enderecoEstab.innerHTML = `Endereço: ${response.data.data.address}. CEP: ${response.data.data.zip_code}`;
+        descricao.innerHTML = response.data.data.description;
+        horario.innerHTML = `Das 12:00 às 18:00`; //TODO: Mockado
+        titulo.innerHTML = response.data.data.name;
+        perfilFoto.src = response.data.data.picture_url;
+        planoFundo.src = response.data.data.background_picture_url;
+
+        if(ID !== response.data.data.user_id){
+            novaPost.style.display = 'none';
+            editarEst.style.display = 'none';
+            for(i=0; i<iconeEditar.length; i++){
+                iconeEditar[i].style.display = 'none';
+            }
+        }
 
     } catch (error) {
         console.log(error);
